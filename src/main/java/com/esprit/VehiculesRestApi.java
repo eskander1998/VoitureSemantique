173d8231d55196bc05ae6cba0c883b39d43b920b
Eskander JGUIRIM <47850204+eskander1998@users.noreply.github.com>
2022-10-31 19:50:59 +0100

    first commit

---
+++ src/main/java/com/esprit/VehiculesRestApi.java
@@ -0,0 +1,211 @@
+package com.esprit;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+ 
+
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.jena.ontology.*;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.json.simple.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@RestController
+public class VehiculesRestApi {
+	
+
+    
+    List<JSONObject> listVoitures=new ArrayList();
+    OntModel model = null;
+    public OntModel readModel() {
+    	String fileName = "data/voiture.owl";
+        try {
+            File file = new File(fileName);
+            FileReader reader = new FileReader(file);
+            OntModel model = ModelFactory
+                    .createOntologyModel(OntModelSpec.OWL_DL_MEM);
+            model.read(reader,null);
+           
+            return model;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+   
+    
+    
+   
+    
+    
+    @RequestMapping(value = "/getVehicules",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<JSONObject> queryGetallInstance() {
+    	
+		   
+        List<JSONObject> list=new ArrayList();
+        try {
+            this.model = this.readModel();
+
+            String querygetPays =
+   	    	     "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +        
+   	    	     "PREFIX vec: <http://www.semanticweb.org/skander/ontologies/2022/9/untitled-ontology-12#>  " +
+   	    	     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+
+   	    	     " SELECT ?voiture  ?nomVoiture ?prixVoiture  " +
+   	    	     " WHERE { ?voiture vec:nomVoiture ?nomVoiture . ?voiture vec:prixVoiture ?prixVoiture .  } " ;
+           
+            //Query query = QueryFactory.create(req1);
+            QueryExecution qe = QueryExecutionFactory.create(querygetPays, this.model);
+            ResultSet resultSet = qe.execSelect();
+           int x=0;
+            while (resultSet.hasNext()) {
+                x++;
+                JSONObject obj = new JSONObject();
+                QuerySolution solution = resultSet.nextSolution();
+                //System.out.println(solution.get("x").toString());
+                obj.put("id",x);
+
+                obj.put("label",solution.get("voiture").toString().substring(solution.get("voiture").toString().indexOf('#')+1));
+                //obj.put("type",solution.get("type").toString().substring(solution.get("type").toString().indexOf('#')+1));
+                obj.put("nomVoiture",solution.get("nomVoiture").toString().substring(solution.get("nomVoiture").toString().indexOf('#')+1));
+                obj.put("prixVoiture",solution.get("prixVoiture").toString());
+//                obj.put("nombredePorte",solution.get("nbporte").toString().substring(0, 1));
+                //obj.put("property",solution.get("nom").toString());
+                //obj.put("object",solution.get("z").toString());
+                list.add(obj);
+            }
+            listVoitures = list ;
+            System.out.println(x);
+            return listVoitures;
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    
+    
+    
+    @RequestMapping(value = "/getMarque",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<JSONObject> queryMarque() {
+    	
+		   
+        List<JSONObject> list=new ArrayList();
+        try {
+            this.model = this.readModel();
+
+            String querygetPays =
+            		 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +        
+               	    	     "PREFIX vec: <http://www.semanticweb.org/skander/ontologies/2022/9/untitled-ontology-12#>  " +
+               	    	     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+
+   	    	     " SELECT ?marque  ?nomMarque ?gammeMarque " +
+   	    	     " WHERE { ?marque vec:nomMarque ?nomMarque . ?marque vec:gammeMarque ?gammeMarque .  } " ;
+           
+            //Query query = QueryFactory.create(req1);
+            QueryExecution qe = QueryExecutionFactory.create(querygetPays, this.model);
+            ResultSet resultSet = qe.execSelect();
+           int x=0;
+            while (resultSet.hasNext()) {
+                x++;
+                JSONObject obj = new JSONObject();
+                QuerySolution solution = resultSet.nextSolution();
+                //System.out.println(solution.get("x").toString());
+                obj.put("id",x);
+
+                obj.put("label",solution.get("marque").toString().substring(solution.get("marque").toString().indexOf('#')+1));
+
+                obj.put("nomMarque",solution.get("nomMarque").toString().substring(solution.get("nomMarque").toString().indexOf('#')+1));
+                obj.put("gammeMarque",solution.get("gammeMarque").toString().substring(solution.get("gammeMarque").toString().indexOf('#')+1));
+
+                list.add(obj);
+            }
+            listVoitures = list ;
+            System.out.println(x);
+            return listVoitures;
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/hautegame",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<JSONObject> getHauteGames() {
+    	
+		   
+        List<JSONObject> list=new ArrayList();
+        String fileName = "data/voiture.owl";
+        try {
+            File file = new File(fileName);
+            FileReader reader = new FileReader(file);
+            OntModel model = ModelFactory
+                    .createOntologyModel(OntModelSpec.OWL_DL_MEM);
+            model.read(reader,null);
+            String querygetPays =
+            		 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +        
+               	    	     "PREFIX vec: <http://www.semanticweb.org/skander/ontologies/2022/9/untitled-ontology-12#>  " +
+               	    	     "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+
+   	    	     " SELECT ?HauteDeGamme ?nomMarque " +
+   	    	     " WHERE { ?HauteDeGamme vec:nomMarque ?nomMarque    } " ;
+            QueryExecution qe = QueryExecutionFactory.create(querygetPays, model);
+            ResultSet resultSet = qe.execSelect();
+           int x=0;
+            while (resultSet.hasNext()) {
+                x++;
+                JSONObject obj = new JSONObject();
+                QuerySolution solution = resultSet.nextSolution();
+                obj.put("id",x);
+                System.out.println(solution);
+              
+                System.out.println(solution.get("nomMarque").toString().substring(solution.get("nomMarque").toString().indexOf('#')+1));
+                obj.put("label",solution.get("HauteDeGamme").toString().substring(solution.get("HauteDeGamme").toString().indexOf('#')+1));
+               obj.put("nomMarque",solution.get("nomMarque").toString().substring(solution.get("nomMarque").toString().indexOf('#')+1));
+        
+          
+                list.add(obj);
+            }
+            System.out.println(x);
+            return list;
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    
+    
+}
+
